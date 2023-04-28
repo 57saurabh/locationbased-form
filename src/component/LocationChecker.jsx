530f0b8e5b3fc1d886c2084bbdba1eebf5390a30
Saurabh Srivastava <Saurabh.20B0131057@abes.ac.in>
2023-04-28 10:06:30 +0530

    final commit

---
+++ src/component/LocationChecker.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Forms from './Forms';
+import MassageError from './MassageError';
+
+
+function LocationChecker() {
+    const [location, setLocation] = useState({latitude:'',longitude:''});
+  
+  const Clat =   28.634352;            //college ka location
+  const Clongi = 77.445845;
+  // const Clat = 28.5099937;            //flat ka location
+  // const Clongi = 77.2354845;
+  const maxlat =   29.634353;
+  const maxLongi = 77.445845;
+  const distance = Math.sqrt(Math.pow((maxlat - Clat), 2) + Math.pow((maxLongi - Clongi), 2));
+  
+  
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+      setLocation('The Browser Does not Support Geolocation');
+    }
+  }, []);
+
+  const showPosition = (position) => {
+    let value ={
+       latitude: position.coords.latitude,
+       longitude: position.coords.longitude
+    }
+    setLocation(location => ({
+       ...location,
+       ...value
+     }));
+};
+// const userdistance = Math.sqrt(Math.pow((position.coords.latitude - Clat), 2) + Math.pow((position.coords.longitude - Clongi), 2));
+const userdistance = Math.sqrt(Math.pow((location.latitude - Clat), 2) + Math.pow((location.longitude - Clongi), 2));
+
+  const showError = (error) => {
+    if (error.PERMISSION_DENIED) {
+      setLocation('The User have denied the request for Geolocation.');
+    }
+  };
+
+  console.log(distance)
+  console.log(userdistance)
+    console.log(location.latitude)
+    console.log(location.longitude)
+
+  return (
+    <div id="location">
+      {/* {location} */}
+
+      {
+         (userdistance <= distance)?<Forms/>:
+           <MassageError/>
+          }
+          
+      
+    </div>
+  )
+}
+
+export default LocationChecker
---
+++ src/component/MassageError.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './massageerror.css'
+
+function MassageError() {
+  return (
+    <div className='messageerror' style={{height:'100vh',width:'100%'}}>
+        <div className='messageerror__container'>
+            <h1>Sorry, You are not in the given range of campus <>:(</></h1><br/>
+            <h2>Try again after reaching the campus</h2>
+            </div>
+
+
+</div>
+  )
+}
+
+export default MassageError
